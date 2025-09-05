601fe2bf48397e037b3fd51a0448c7523f8d2dbe
andre <andre@ANDREW>
2025-09-05 22:41:11 +0800

    Add project files.

---
+++ A. watermelon/FileName.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int w, sum, i = 2, r, ctr = 0;
+	printf("Enter the weight of the watermelon: ");
+	scanf_s("%d", &w);
+	while (w > ctr) {
+		if (w % i == 0) {
+			r = w / i;
+			printf("the boys can divide the watermelon into %.0d parts and %.0d weight \n", i, r);
+			i = i + 2;
+		}
+		else if (w % i != 0) {
+			printf("the boys cannot divide the watermelon into equal parts:<");
+			break;
+		}
+		ctr++;
+
+	}
+	return 0;
+}
